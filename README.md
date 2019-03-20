0b80aedcb166ed0f6bb255d6feea25559fcf04aa
Gareth Hay <48715357+garethhay@users.noreply.github.com>
2019-03-20 12:39:31 +0000

    Cleanup and add login_getwlans.py

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# SmartzoneAPI
+# Ruckus SmartZone API
 
-Repo for some basic python scripts for pulling data and printing data from Ruckus Smartzone's API.
+Repo for some basic python scripts for pulling data and printing data from Ruckus SmartZone's API.
 
 Please note I am writing these as a training exercise for myself. Both new to python, git and has been a long time since I have coded.
 
 Tested against a SZ100 cluster of 2 running 3.6.2, please note these scripts will not work as is with vSZ-H only vSZ-E. Would recommend using a read-only account.
 
-Each script will need the below variables modifying for your own environment:
+### Each script will need the below variables modifying for your own environment:
 
 * `baseurl`
     * "https://general.direction.com:8443/wsg/api/public/v6_1/" #replace "general.direction.com" with either the host name or IP of a member of the cluster
@@ -26,3 +26,11 @@ This will return all the configured zones on the cluster and print them out. At 
 Format of the lists are:
 
 `[name_of_zone,zone_id]`
+
+## login_getwlans.py
+
+This script builds on `login_getzone.py` to take the `cleaned_zones` list of lists and produce a list of lists called `cleaned_all_zone_wlan` that also includes the WLAN names and ids. Please note this is the WLAN name NOT the SSID.
+
+Format of the lists are:
+
+`[name_of_zone,zone_id,name_of_WLAN,WLAN_id]`
--- login_getwlans.py
+++ login_getwlans.py
@@ -7,6 +7,7 @@ jar = requests.cookies.RequestsCookieJar()
 baseurl = "https://general.direction.com:8443/wsg/api/public/v6_1/" #replace "general.direction.com" with either the host name or IP of a member of the cluster
 
 # Written with 3.6.2 in mind
+
 #http://docs.ruckuswireless.com/smartzone/3.6.2/sz100-public-api-reference-guide-3-6-2.html API documentation
 
 szusername = "" #Enter a username with read privages to everything you want to access
@@ -49,22 +50,39 @@ def clean_ruckus_list(dictdata,dict_parent_name = "NONE",dict_parent_id = "NONE"
                 output_id = row[key]
             elif key == names:
                 output_name = row[key]
-        if dict_parent_name and dict_parent_id == "NONE": #Produce a list without useless data but catchs if someone doesn't pass both arguements
-            output.append([output_name,output_id])
+        if dict_parent_name and dict_parent_id == "NONE":
+            output.append([output_name,output_id]) #Produce a list without useless data but catch if someone doesn't pass both arguements
         else:
             output.append([dict_parent_name,dict_parent_id,output_name,output_id])
     return output
 
-
 cleaned_zones = clean_ruckus_list(zones)
+
+cleaned_all_zone_wlan = []
+
+for row in cleaned_zones:
+    zone_id = row[1]
+    zone_name = row[0]
+    urltemplate = "rkszones/{}/wlans"
+    jsonwlan = ruckus_get(urltemplate.format(zone_id))
+    wlan = ruckus_list(jsonwlan)
+    cleaned_all_zone_wlan.extend(clean_ruckus_list(wlan,zone_name,zone_id))
 
 print("\n")
 print("-" * 50)
 print("\n")
-print("The AP zones configured on this szcluster are:")
+print("The WLANs configured on this szcluster are:")
 print("\n")
-for row in cleaned_zones:
-    print("Name: {} and ID: {}".format(row[0],row[1]))
-    print("-" * 5)
+zone_print = ""
+for row in cleaned_all_zone_wlan:
+    if zone_print == row[0]:
+        print("    Name: {} and ID: {}".format(row[2],row[3]))
+    else:
+        zone_print = row[0]
+        print("-" * 5)
+        print("\n")
+        print("{} zone's WLAN are:".format(row[0]))
+        print("\n")
+        print("    Name: {} and ID: {}".format(row[2],row[3]))
 print("\n")
 print("-" * 50)
