29904ee88c73cf78955d8ea5f9a3d187fdf7bf24
Gareth Hay <48715357+garethhay@users.noreply.github.com>
2019-03-25 13:50:55 +0000

    Add login_getwlans_compare.py

--- README.md
+++ README.md
@@ -5,22 +5,27 @@ Repository for basic python scripts to pull and print data from Ruckus SmartZone
 # Note
 
 I am new to both python and git and scripts are training exercises. I have not written code for an extended period.
-Program is tested against a SZ100 cluster of 2 running 3.6.2. These scripts work correctly with vSZ-E but not with vSZ-H. Read-only account is reccomended. 
+Program is tested against a SZ100 cluster of 2 running 3.6.2. These scripts work correctly with vSZ-E but not with vSZ-H. Read-only account is reccomended.
 
 ### Each script will need the following variables which will need modifying for your own configuration:
 
 * `baseurl`
-    * "https://general.direction.com:8443/wsg/api/public/v6_1/" # replace "general.direction.com" with the host name or IP of a member of the cluster
+    * "https://general.direction.com:8443/wsg/api/public/v6_1/" replace "general.direction.com" with the host name or IP of a member of the cluster
 * `szusername`
     * String
 * `szpassword`
     * String
+* `check_cert`
+    * Boolean - set to False if using selfsigned certificates or certificate chain is not installed on the machine
+    * There is also an import commented out for supressing invalid certificate warnings for testing purposes only.
 
 Use API documentation below for reference:
 
 [Ruckus documentation for version 3.6.2](http://docs.ruckuswireless.com/smartzone/3.6.2/sz100-public-api-reference-guide-3-6-2.html)
 
 ## login_getzone.py
+
+Prerequisites: requests, json
 
 Returns and prints all the configured zones on the cluster. The script will output a list of lists called `cleaned_zones`.
 
@@ -30,6 +35,8 @@ Format of the lists are:
 
 ## login_getwlans.py
 
+Prerequisites: requests, json
+
 Builds on `login_getzone.py` to produce a list of lists called  `cleaned_all_zone_wlan`  from  `cleaned_zones`  that includes the WLAN names and ids. Please note this is the WLAN name NOT the SSID.
 
 Format of the lists are:
@@ -38,6 +45,18 @@ Format of the lists are:
 
 ## login_getwlan_details.py
 
+Prerequisites: requests, json, sys
+
 Returns and prints the JSON from individual WLAN. If  `wlan_template_id`  or `zone_template_id`  are blank the program will ask for the names of the WLAN and the zone to be inputted. The script attempts to account for spelling mistakes in the WLAN name before quitting if WLAN is not found in the zone.
 
 It will save the WLAN's details in a variable called `wlan_template`.
+
+## login_getwlans_compare.py
+
+Prerequisites: requests, json, sys, dictdiffer
+
+Returns and prints the differences in configuration of two different WLANs. Useful for quickly solving why a certain WLAN isn't working as expected. It builds on `login_getwlan_details` but will need two sets of WLAN + Zone details, either one template and one inputted or both manually inputted. The ignore list removes common and expected differences as defined below, can be used or not.
+
+`ignore_list = ['id','name','ssid','encryption.passphrase','bssid','zoneId','description']`
+
+The results are saved in `wlans_compared`. The format is in the helper module documentation [here](https://dictdiffer.readthedocs.io/en/latest/).
---
+++ login_getwlans_compare.py
@@ -0,0 +1,165 @@
+import requests
+import json
+import sys
+from dictdiffer import diff
+
+# Only use when testing, surpresses warnings about insecure servers
+#from requests.packages.urllib3.exceptions import InsecureRequestWarning
+#requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+session = requests.Session()
+jar = requests.cookies.RequestsCookieJar()
+
+baseurl = "https://general.direction.com:8443/wsg/api/public/v6_1/" #replace "general.direction.com" with either the host name or IP of a member of the cluster
+
+# Written with 3.6.2 in mind
+
+#http://docs.ruckuswireless.com/smartzone/3.6.2/sz100-public-api-reference-guide-3-6-2.html API documentation
+
+sz_username = "" #Enter a username with read privages to everything you want to access
+sz_password = "" #Password for the above account
+check_cert = True # Change to false if using selfsigned certs or cert chain is not on the machine running the script
+
+headers_template = {'Content-Type': "application/json;charset=UTF-8"}
+
+login_payload = '{  "username": "' + sz_username + '",\r\n  "password": "' + sz_password + '"}'
+
+wlan_template_id = ""
+zone_template_id = ""
+
+def ruckus_post(url,data,headers = headers_template):
+    output = session.post(baseurl + url, data=data, headers=headers, verify=check_cert, cookies=jar)
+    return output
+
+get_login_session_cookie = ruckus_post("session",login_payload) #This uses the ruckus_post above to get a session valid session cookie into the cookie jar
+
+
+def ruckus_get(url,headers = headers_template):
+    output = session.get(baseurl + url, headers=headers, verify=check_cert, cookies=jar)
+    return output
+
+jsonzones = ruckus_get("rkszones") #Get the JSON data for the zones confiured on the cluster
+
+#The below function ruckus_list is used for stripping out the "list" dictionary from the returned JSON
+def ruckus_list(jsondata):
+    output = {}
+    output = json.loads(jsondata.text)
+    output = output["list"]
+    return output
+
+zones = ruckus_list(jsonzones)
+
+def clean_ruckus_list(dictdata,dict_parent_name = "NONE",dict_parent_id = "NONE",names="name",ids="id"):
+    output = []
+    for row in dictdata:
+        output_name = ""
+        output_id = ""
+        for key,val in row.items():
+            if key == ids:
+                output_id = row[key]
+            elif key == names:
+                output_name = row[key]
+        if dict_parent_name and dict_parent_id == "NONE":
+            output.append([output_name,output_id]) #Produce a list without useless data but catch if someone doesn't pass both arguements
+        else:
+            output.append([dict_parent_name,dict_parent_id,output_name,output_id])
+    return output
+
+cleaned_zones = clean_ruckus_list(zones)
+
+cleaned_all_zone_wlan = []
+
+for row in cleaned_zones:
+    zone_id = row[1]
+    zone_name = row[0]
+    urltemplate = "rkszones/{}/wlans"
+    jsonwlan = ruckus_get(urltemplate.format(zone_id))
+    wlan = ruckus_list(jsonwlan)
+    cleaned_all_zone_wlan.extend(clean_ruckus_list(wlan,zone_name,zone_id))
+
+
+# Below is ugly and needs rewiritng as a While statement
+# Get input, check input exists, if doesn't try first 4
+def wlan_input():
+    output = []
+    zone_input_id = ""
+    wlan_input_id = ""
+    print('-' * 50)
+    print('\n')
+    wlan_input_name = input("Please enter a WLAN name:")
+    zone_input_name = input("Please enter the zone the WLAN is part of:")
+    print("-" * 5)
+    wlan_input_name = wlan_input_name.lower()
+    zone_input_name = zone_input_name.lower()
+    tested = 0
+    for row in cleaned_all_zone_wlan:
+        tested += 1
+        wlan_compare_input = row[2]
+        zone_compare_input = row[0]
+        wlan_compare_input = wlan_compare_input.lower()
+        zone_compare_input = zone_compare_input.lower()
+        if wlan_input_name == wlan_compare_input and zone_input_name == zone_compare_input:
+            print("Using {} from {}.".format(wlan_input_name,zone_input_name))
+            wlan_input_id = row[3]
+            zone_input_id = row[1]
+            output = zone_input_id, wlan_input_id, wlan_compare_input
+            return output
+        elif wlan_input_name[:4] in wlan_compare_input and zone_input_name == zone_compare_input:
+            confirm = input("Did you mean {} from {}? Y/N".format(row[2],row[0]))
+            if confirm.lower() == "y":
+                wlan_input_id = row[3]
+                zone_input_id = row[1]
+                output = zone_input_id, wlan_input_id, wlan_compare_input
+                return output
+            else:
+                sys.exit("WLAN name not found")
+    sys.exit("Zone or WLAN name not found")
+
+# Check to confirm we have an id for both the WLAN and it's zone before continuing and check
+# the WLAN is actually in that zone it was entered directly
+# Further checks due to possible assigned template
+template_wlan_in_zone = False
+if wlan_template_id == "" or zone_template_id =="":
+    print('-' * 100)
+    print('\n')
+    print("For the template WLAN:")
+    zone_template_id, wlan_template_id, wlan_template_name = wlan_input()
+else:
+    for row in cleaned_all_zone_wlan:
+        if zone_template_id == row[1] and wlan_template_id == row [3]:
+            template_wlan_in_zone = True
+            break
+        else:
+            if template_wlan_in_zone != True:
+                sys.exit("WLAN id not in Zone id")
+            elif wlan_template_id == "":
+                sys.exit("No WLAN id")
+            elif zone_template_id == "":
+                sys.exit("No Zone id to use with the WLAN")
+
+# Get the second WLAN and Zone id
+print("\n")
+print("For the comparison WLAN:")
+zone_comparison_id, wlan_comparison_id, wlan_comparison_name = wlan_input()
+print("\n")
+print("-" * 100)
+#Do the get and JSON.loads to receive both WLAN details
+wlan_template_json = ruckus_get("rkszones/{}/wlans/{}".format(zone_template_id,wlan_template_id))
+wlan_comparison_json = ruckus_get("rkszones/{}/wlans/{}".format(zone_comparison_id,wlan_comparison_id))
+
+wlan_template = json.loads(wlan_template_json.text)
+wlan_comparison = json.loads(wlan_comparison_json.text)
+
+ignore_list = ['id','name','ssid','encryption.passphrase','bssid','zoneId','description']
+
+wlans_compared = diff(wlan_template,wlan_comparison, ignore = ignore_list)
+
+print("\n")
+print("The differences between '{}' and '{}' are:".format(wlan_template_name,wlan_comparison_name))
+print("\n")
+for row in wlans_compared:
+    print("Type: {} | Where: {} | What: {}".format(row[0],row[1],row[2]))
+    print("-" * 5)
+print("\n")
+print("-" * 100)
